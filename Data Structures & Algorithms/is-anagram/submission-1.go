de96da6a37e2e2f312034c07b509414989622ac6
NIKHIL B <nikhilbkashyap55@gmail.com>
2026-09-12 15:23:19 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.go
@@ -0,0 +1,17 @@
+func isAnagram(s string, t string) bool {
+	x,y:= make(map[byte]int),make(map[byte]int)
+	if len(s)!=len(t){
+		return false
+	}else{
+		for i:=0;i<len(s);i++{
+			x[s[i]]++
+			y[t[i]]++
+		}
+	}
+	for i,v:= range x{
+		if y[i]!= v{
+			return false
+		}		
+	}		
+	return true
+}
